f18c69fe4c2d45af62c2490d61834e471faa3358
Seungminin <empe8339@gmail.com>
2025-07-08 17:17:24 +0900

    CTAB-GAN 300 epochs

--- CTAB-GAN/main.py
+++ CTAB-GAN/main.py
@@ -3,11 +3,11 @@ from model.ctabgan import CTABGAN
 if __name__ == "__main__":
     model = CTABGAN(
         raw_csv_path="Real_Datasets/train_category_1.csv",
-        epochs=3
+        epochs=300
     )
     
     model.fit()
     
     synthetic_df = model.generate_samples(N_CLS_PER_GEN=540000)
     output_path = "C:/Users/GCU/Lending_club/Data_Analysis_lending-club/CTAB-GAN/Fake_Datasets/"
-    synthetic_df.to_csv(path_or_buf=output_path+"ctab-gan.csv", index=False)
+    synthetic_df.to_csv(path_or_buf=output_path+"ctab-gan_300epochs.csv", index=False)
