f2a2edf67030025b7b1ff1dfdebce210fad19640
Seungminin <empe8339@gmail.com>
2025-07-09 18:27:54 +0900

    smotified-ctab-gan

--- CTAB-GAN/main.py
+++ CTAB-GAN/main.py
@@ -2,7 +2,7 @@ from model.ctabgan import CTABGAN
 
 if __name__ == "__main__":
     model = CTABGAN(
-        raw_csv_path="Real_Datasets/train_category_1.csv",
+        raw_csv_path="Real_Datasets/smotifiedCTGAN_data_class1.csv",
         epochs=300
     )
     
@@ -10,4 +10,4 @@ if __name__ == "__main__":
     
     synthetic_df = model.generate_samples(N_CLS_PER_GEN=540000)
     output_path = "C:/Users/GCU/Lending_club/Data_Analysis_lending-club/CTAB-GAN/Fake_Datasets/"
-    synthetic_df.to_csv(path_or_buf=output_path+"ctab-gan_300epochs.csv", index=False)
+    synthetic_df.to_csv(path_or_buf=output_path+"smotified_ctabgan.csv", index=False)
